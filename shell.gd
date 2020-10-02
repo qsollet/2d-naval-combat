6ed005c6eceacdd444f5d61486ab8d848415aa23
Lightjohn <johnleger26@gmail.com>
2020-10-02 20:36:38 +0100

    playing with target

--- shell.gd
+++ shell.gd
@@ -4,15 +4,14 @@ extends RigidBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-export var speed = 200
+export var speed = 400
 var direction = Vector2(0.0, -1.0)
-var rotation_deg = 85
+var rotation_deg = 78
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	var rot_dir = direction.rotated(deg2rad(rotation_deg))
-	print(rot_dir)
 	self.rotate(deg2rad(rotation_deg))
 	self.set_linear_velocity(rot_dir*speed)
 
--- target.gd
+++ target.gd
@@ -1,16 +1,35 @@
-extends Node2D
-
+extends Area2D
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var text
+var timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for child in get_children():
+		if child is RichTextLabel:
+		  text = child
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
+
+
+func _on_target_body_shape_entered(body_id, body, body_shape, local_shape):
+	text.push_color( Color.red )
+	text.set_text("BOOM")
+	timer = Timer.new()
+	add_child(timer)
+	timer.set_wait_time(0.5)
+	timer.connect("timeout", self, "_on_timer_timeout") 
+	timer.start()
+	
+	
+func _on_timer_timeout():
+	print("Timeout")
+	text.set_text("")
+	timer.stop()
+
