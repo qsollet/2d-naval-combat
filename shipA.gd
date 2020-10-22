a18ff9319a03c5a74fa8c669003104ec5c65e3d0
Quentin Sollet <quentin@sollet.fr>
2020-10-22 17:46:58 +0200

    moved turret related code to turret script

--- shipA.gd
+++ shipA.gd
@@ -1,41 +1,17 @@
 extends RigidBody2D
 
-export (PackedScene) var Shell
 export var speed = 10
-export var fire_timeout = 2
 var target = null
-var turret_rotation_speed = 1
-var can_shoot = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$ShootTimer.wait_time = self.fire_timeout
 	var elems = get_tree().get_nodes_in_group("target")
 	for elem in elems:
 		target = elem
-
-# function to rotate the turret
-func aim(delta):
-	if target:
-		# TODO fix seem to aim from the center of the ship instead of the turret
-		var target_rot = $turret1.get_angle_to(target.global_position)
-		if abs(target_rot) > 0.01:
-			var max_rot = delta * self.turret_rotation_speed
-			if max_rot > abs(target_rot):
-				$turret1.rotation += target_rot
-			elif target_rot > 0:
-				$turret1.rotation += max_rot
-			else:
-				$turret1.rotation -= max_rot
+	$turret1.target = target
 
 func shoot():
-	if target and can_shoot:
-		print("shoot")
-		var b = Shell.instance()
-		b.start($turret1.global_position, $turret1.global_rotation)
-		get_parent().add_child(b)
-		can_shoot = false
-		$ShootTimer.start()
+	$turret1.shoot()
 
 func _process(delta):
 	# Ship movement
@@ -50,13 +26,6 @@ func _process(delta):
 		velocity.y -= 1
 	self.position += velocity * self.speed * delta
 	
-	# Turret aim
-	aim(delta)
-	
 	# Shoot
 	if Input.is_action_pressed("ui_accept"):
 		shoot()
-
-func _on_ShootTimer_timeout():
-	print("reloaded")
-	can_shoot = true
---
+++ turret.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+
+export (PackedScene) var Shell
+export var rotation_speed = 1.5
+export var fire_timeout = 2
+var target = null
+var can_shoot = true
+
+
+func _ready():
+	$ShootTimer.wait_time = self.fire_timeout
+
+
+func _process(delta):
+	aim(delta)
+
+
+func aim(delta):
+	if target:
+		var target_rot = get_angle_to(target.global_position)
+		if abs(target_rot) > 0.01:
+			var max_rot = delta * self.rotation_speed
+			if max_rot > abs(target_rot):
+				rotation += target_rot
+			elif target_rot > 0:
+				rotation += max_rot
+			else:
+				rotation -= max_rot
+
+
+func shoot():
+	if target and can_shoot:
+		var b = Shell.instance()
+		b.start(position, rotation)
+		#get_tree().get_root().add_child(b)
+		get_parent().add_child(b)
+		can_shoot = false
+		$ShootTimer.start()
+
+
+func _on_ShootTimer_timeout():
+	can_shoot = true
